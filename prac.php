03649fecb09c6dc720fc5dd5410e1b20c6e911e5
yaoyue0804 <sunny920804@gmail.com>
2023-11-12 22:59:51 +0800

    1

---
+++ prac.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>哇哈哈哈</title>
+</head>
+<body>
+    <h1>挖</h1>
+    <b>挖</b>
+    <a target="_blank" href="https://www.youtube.com/" title="UCCU">Youtubeeeee</a>
+    <!--target="_blank" 新分頁開啟
+    //title 小提示-->
+    <p>喔齁</p>
+    <p>喔齁齁</p>
+    <p>喔齁齁齁</p>
+    
+    <ul>
+        <li>totto</li>
+        <li>kawai</li>
+        <li>tossy</li>
+        <li>kakui</li>
+    </ul>
+    <ol>
+        <li>totto</li>
+        <li>kawai</li>
+        <li>tossy</li>
+        <li>kakui</li>
+    </ol>
+
+    <img src="wert.jpg" alt="诶黑">
+    <img src="https://www.nuk.edu.tw/var/file/0/1000/img/147/logo.png" alt="">
+</body>
+</html>
